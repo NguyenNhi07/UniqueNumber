e49c2292be110b8436b43e844de8648287fdbf48
NguyenNhi07 <nguyennhi.hau2003@gmail.com>
2024-04-08 18:52:29 +0700

    uniquenumber

---
+++ UniqueNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+namespace UniqueNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = { 4, 3, 6, 3, 6 };
+            int uniqueNumber = FindNumber(Numbers);
+            if (uniqueNumber != 0)
+            {
+                Console.Write("Số duy nhất trong mảng là: " + uniqueNumber);
+            }
+            else
+            {
+                Console.Write("Null");
+            }
+        }
+        static int FindNumber(int[] Numbers)
+        {
+            HashSet<int> unique = new HashSet<int>();
+            HashSet<int> repeat = new HashSet<int>();
+            foreach (int number in Numbers)
+            {
+                if (!unique.Add(number))
+                {
+                    unique.Remove(number);
+                    repeat.Add(number);
+                }
+                else if (!repeat.Contains(number))
+                {
+                    unique.Add(number);
+                }
+            }
+            foreach (int uniqueNumber in unique)
+            {
+                return uniqueNumber;
+            }
+            return 0;
+        }
+    }
+}
